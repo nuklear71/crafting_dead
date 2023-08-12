677bd637aa042d823ac6a570debd115bea6eb7c4
nuklear71 <nuklear710@gmail.com>
2023-08-13 01:09:25 +0200

    creative tab orginazd

--- src/main/java/net/crafting_dead/crafting_dead/item/ModCreativeModTabs.java
+++ src/main/java/net/crafting_dead/crafting_dead/item/ModCreativeModTabs.java
@@ -64,25 +64,24 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.ROCKY_DIRT_SLAB.get());
 
                         pOutput.accept(ModBlocks.STONE_1.get());
-                        pOutput.accept(ModBlocks.STONE_1_SLAB.get());
                         pOutput.accept(ModBlocks.STONE_1_STAIRS.get());
+                        pOutput.accept(ModBlocks.STONE_1_SLAB.get());
                         //
                         pOutput.accept(ModBlocks.STONE_2.get());
-                        pOutput.accept(ModBlocks.STONE_2_SLAB.get());
                         pOutput.accept(ModBlocks.STONE_2_STAIRS.get());
+                        pOutput.accept(ModBlocks.STONE_2_SLAB.get());
                         //
                         pOutput.accept(ModBlocks.STONE_3.get());
-                        pOutput.accept(ModBlocks.STONE_3_SLAB.get());
                         pOutput.accept(ModBlocks.STONE_3_STAIRS.get());
+                        pOutput.accept(ModBlocks.STONE_3_SLAB.get());
                         //
                         pOutput.accept(ModBlocks.BLACK_WHITE_TILES_SMALL.get());
-                        pOutput.accept(ModBlocks.BLACK_WHITE_TILES_SMALL_SLAB.get());
                         pOutput.accept(ModBlocks.BLACK_WHITE_TILES_SMALL_STAIRS.get());
+                        pOutput.accept(ModBlocks.BLACK_WHITE_TILES_SMALL_SLAB.get());
                         //
                         pOutput.accept(ModBlocks.CRACKED_BLACK_WHITE_TILES_SMALL.get());
-                        pOutput.accept(ModBlocks.CRACKED_BLACK_WHITE_TILES_SMALL_SLAB.get());
                         pOutput.accept(ModBlocks.CRACKED_BLACK_WHITE_TILES_SMALL_STAIRS.get());
-
+                        pOutput.accept(ModBlocks.CRACKED_BLACK_WHITE_TILES_SMALL_SLAB.get());
                     })
                     .build());
 
